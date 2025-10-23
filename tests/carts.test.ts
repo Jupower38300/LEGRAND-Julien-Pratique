174c70de81bb8dfe6d4f1738e6d0ce3ef9ccdd4e
Jupower38300 <julienlegrand038@gmail.com>
2025-10-23 15:00:29 +0200

    Ajout des tests pour enlever des produits

--- tests/carts.test.ts
+++ tests/carts.test.ts
@@ -1,10 +1,27 @@
 import { Carts } from "../src/carts";
 
 describe("Ajout de produits dans le panier", () => {
+  let cart: Carts;
+
+  beforeEach(() => {
+    cart = new Carts();
+  });
+
   it("test_getTotal_AddingProductsInCart_ShouldReturnTotal", () => {
-    const cart = new Carts();
     cart.addProduct("Bonbon", 3);
     cart.addProduct("Sucette", 2);
     expect(cart.getTotal()).toBe(5);
+  });
+
+  it("test_removeProduct_RemovingAProduct_ShouldLowerTheTotal", () => {
+    cart.addProduct("Bonbon", 3);
+    cart.addProduct("Sucette", 2);
+    cart.removeProduct("Sucette");
+    expect(cart.getTotal()).toBe(3);
+  });
+
+  it("test_removeProduct_RemovingSomethingThatDoesntExist_ShouldNotThrowError", () => {
+    cart.addProduct("Bonbon", 3);
+    expect(() => cart.removeProduct()).not.toThrow();
   });
 });
