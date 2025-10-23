83e7c79e2b788697c9c149be9cd85154d238d696
Jupower38300 <julienlegrand038@gmail.com>
2025-10-23 15:12:12 +0200

    Ajout de la fonction de mise en place de réductions

--- src/carts.ts
+++ src/carts.ts
@@ -1,18 +1,22 @@
 export class Carts {
   products: { name: string; price: number }[] = [];
 
+  discount: number = 0;
+
   addProduct(name: string, price: number): void {
     this.products.push({ name, price });
   }
 
   getTotal(): number {
-    return this.products.reduce(
-      (accumulator, currentValue) => accumulator + currentValue.price,
-      0,
-    );
+    const total = this.products.reduce((acc, prod) => acc + prod.price, 0);
+    return +(total * (1 - this.discount / 100)).toFixed(2);
   }
 
   removeProduct(name: string): void {
     this.products = this.products.filter((product) => product.name !== name);
+  }
+
+  applyDiscount(pourcent: number): void {
+    this.discount = pourcent;
   }
 }
